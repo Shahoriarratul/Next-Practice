c92ded9397401bab1b3e60f6e1cb2986fc3865fd
Ratul <shahoriar.ratul@gmail.com>
2024-02-04 01:25:48 +0600

    build(deps): delete 2 files and update 2 files

---
+++ first-app/app/page.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <section>
+      <div className="container">
+        <h1 className="text-3xl font-bold">Nextjs 14 starter kit</h1>
+      </div>
+    </section>
+  );
+}
