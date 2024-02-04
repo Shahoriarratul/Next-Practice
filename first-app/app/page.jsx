067c6ba43b01df3a68dac567a48a661252f6813a
Ratul <shahoriar.ratul@gmail.com>
2024-02-04 15:16:08 +0600

    adas

--- first-app/app/page.jsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <section>
-      <div className="container">
-        <h1 className="text-3xl font-bold">Nextjs 14 starter kit</h1>
-      </div>
-    </section>
-  );
-}
