7b104699f11634607983c867178ac6d442f66c40
Ratul <shahoriar.ratul@gmail.com>
2024-02-07 18:51:35 +0600

    assed asset and layout

--- first-app/app/layout.tsx
+++ first-app/app/layout.tsx
@@ -1,13 +1,27 @@
 import { ClerkProvider } from "@clerk/nextjs";
-import "./global.css";
-import React from "react";
+import "./globals.css";
 import { Inter, Space_Grotesk } from "next/font/google";
+import React from "react";
 import type { Metadata } from "next";
 
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
 export const metadata: Metadata = {
-  title: "Next.js 13 with clerk",
+  title: "Dev Flow",
   description:
     "A community-droven platform for asking and answering programming Questions. Get help, share knowledge, and collaborate with devlopers from around the world. Explore topics in the web devloupment, mobile app devloupment, algorithm, dtastructure, and more.",
+  icons: {
+    icon: "/assets/image/site-logo.svg",
+  },
 };
 
 export default function RootLayout({
@@ -16,9 +30,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable}${spaceGrotesk.variable}`}>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
