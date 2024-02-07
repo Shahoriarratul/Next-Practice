9a0cc0415a68eafbe0c043da5a8305f27af28b6c
Ratul <shahoriar.ratul@gmail.com>
2024-02-07 18:20:02 +0600

    update

--- first-app/app/layout.tsx
+++ first-app/app/layout.tsx
@@ -1,6 +1,14 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import "./global.css";
 import React from "react";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Next.js 13 with clerk",
+  description:
+    "A community-droven platform for asking and answering programming Questions. Get help, share knowledge, and collaborate with devlopers from around the world. Explore topics in the web devloupment, mobile app devloupment, algorithm, dtastructure, and more.",
+};
 
 export default function RootLayout({
   children,
